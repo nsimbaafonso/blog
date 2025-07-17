c873609a9a2dd503de17df03275f7114dde6bc47
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-07-17 23:12:25 +0100

    Página de confirmação para deletar atualizada

--- dashboard/newsletter/eliminar.php
+++ dashboard/newsletter/eliminar.php
@@ -1,3 +1,27 @@
+
+<?php 
+session_start();
+include __DIR__ . '/../../config/db.php';
+include __DIR__ . '/../../includes/functions.php';
+
+$newsletter = null;
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    $sql = "SELECT email FROM newsletter WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        $newsletter = $result->fetch_assoc();
+    }
+} else {
+    redirect("/blog/dashboard/newsletter/");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-ao">
 <head>
@@ -25,10 +49,18 @@
 
         <!--form-->
         <section class="section-form">
-            <form class="form" id="form">
+            <?php
+                if (isset($_SESSION['msgs'])) {
+                    foreach ($_SESSION['msgs'] as $msg) {
+                        echo $msg;
+                    }
+                    unset($_SESSION['msgs']);
+                }
+            ?>
+            <form action="actions/delete" method="POST" class="form" id="form">
                 <h3>Eliminar Newsletter?</h3>
-                <p>Tem a certeza que deseja eliminar o newsletter?</p>
-                <input type="hidden" name="" value="" required>
+                <p>Tem a certeza que deseja eliminar o newsletter <?= $newsletter["email"]?>?</p>
+                <input type="hidden" name="idnewsletter" value="<?= $id?>">
                 <div class="d-flex-2">
                     <a href="/blog/dashboard/newsletter" class="btn">Cancelar</a>
                     <button type="submit" class="btn red">Eliminar Newsletter</button>
