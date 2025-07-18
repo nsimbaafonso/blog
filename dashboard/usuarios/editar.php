1b2b266b13beba901b3ff7a0d323dd96e8f3049b
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-07-18 15:08:29 +0100

    Página preparada para editar usuário

--- dashboard/usuarios/editar.php
+++ dashboard/usuarios/editar.php
@@ -1,3 +1,27 @@
+<?php 
+session_start();
+include __DIR__ . '/../../config/db.php';
+include __DIR__ . '/../../includes/functions.php';
+include 'actions/funcao.php';
+
+$usuario = null;
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    $sql = "SELECT nome, sobrenome, email, telefone, status, id_funcao, imagem 
+    FROM usuario WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        $usuario = $result->fetch_assoc();
+    }
+} else {
+    redirect("http://localhost/blog/dashboard/usuarios/");
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-ao">
 <head>
@@ -8,7 +32,6 @@
     <link href="../assets/fonts/fontawesome-6.5.2/css/brands.css?<?= time(); ?>" rel="stylesheet">
     <link href="../assets/fonts/fontawesome-6.5.2/css/solid.css?<?= time(); ?>" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css?<?= time(); ?>">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <div class="container">
@@ -26,33 +49,63 @@
 
         <!--form-->
         <section class="section-form">
-            <form class="form" id="form">
+            <?php
+                if (isset($_SESSION['msgs'])) {
+                    foreach ($_SESSION['msgs'] as $msg) {
+                        echo $msg;
+                    }
+                    unset($_SESSION['msgs']);
+                }
+            ?>
+            <form action="actions/update" method="POST" class="form" id="form">
                 <h3>Editar Usuário</h3>
+                <input type="hidden" name="idusuario" value="<?= $id ?>">
                 <div class="inputBox">
                     <div>
-                        <input type="text" name="nome" placeholder="Nome do usuário"  class="box" required>
+                        <input type="text" name="nome" placeholder="Nome do usuário"  class="box" value="<?= htmlspecialchars($usuario['nome']) ?>">
                     </div>
                 </div>
 
                 <div class="inputBox">
                     <div>
-                        <input type="text" name="sobrenome" placeholder="Sobrenome do usuário"  class="box" required>
+                        <input type="text" name="sobrenome" placeholder="Sobrenome do usuário"  class="box" value="<?= htmlspecialchars($usuario['sobrenome']) ?>">
                     </div>
                 </div>
 
                 <div class="inputBox">
-                    <select name="cat" id="cat" class="box">
+                    <div>
+                        <input type="email" name="email" placeholder="exemplo@gmail.com"  class="box" value="<?= htmlspecialchars($usuario['email']) ?>">
+                    </div>
+                </div>
+
+                <div class="inputBox">
+                    <div>
+                        <input type="tel" name="telefone" placeholder="+244 923 456 789"  class="box" value="<?= htmlspecialchars($usuario['telefone']) ?>">
+                    </div>
+                </div>
+
+                <div class="inputBox">
+                    <select name="funcao" id="funcao" class="box">
                         <option value="" disabled selected>-- Selecione uma função --</option>
-                        <option value="1">Admin</option>
-                        <option value="2">Editor</option>
+                        <?php if(!empty($funcoes)): ?>
+                            <?php foreach ($funcoes as $funcao): ?>
+                                <option value="<?= $funcao['id'] ?>" <?= $usuario['id_funcao'] == $funcao['id'] ? 'selected' : '' ?>>
+                                    <?= $funcao['nome'] ?>     
+                                </option>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <option value="">Nenhuma função disponível</option>
+                        <?php endif; ?>
                     </select>
                 </div>
 
                 <div class="inputBox">
-                    <select name="cat" id="cat" class="box">
+                    <select name="status" id="status" class="box">
                         <option value="" disabled selected>-- Selecione um status --</option>
-                        <option value="0">Inativo</option>
-                        <option value="1">Ativo</option>
+                        <option value="ativo" <?= $usuario['status'] === 'ativo' ? 'selected' : '' ?>>Ativo</option>
+                        <option value="inativo" <?= $usuario['status'] === 'inativo' ? 'selected' : '' ?>>Inativo</option>
+                        <option value="pendente" <?= $usuario['status'] === 'pendente' ? 'selected' : '' ?>>Pendente</option>
+                        <option value="banido" <?= $usuario['status'] === 'banido' ? 'selected' : '' ?>>Banido</option>
                     </select>
                 </div>
 
@@ -61,16 +114,40 @@
         </section>
 
         <section class="section-form">
-            <form class="form" id="form">
-                <h3>Alterar Imagem</h3>
-
+            <form action="actions/update-senha" method="POST" class="form" id="form">
+                <h3>Alterar Senha</h3>
+                <input type="hidden" name="idusuario" value="<?= $id ?>">
                 <div class="inputBox">
                     <div>
-                        <input type="file" name="imagem"  class="box" required>
+                        <input type="password" name="senha" placeholder="Sua senha"  class="box">
                     </div>
                 </div>
 
-                <button type="submit" class="btn">Alterar Imagem</button>
+                <div class="inputBox">
+                    <div>
+                        <input type="password" name="confirmaSenha" placeholder="Confirme sua senha"  class="box">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn">Alterar Senha</button>
+            </form>
+        </section>
+
+        <section class="section-form">
+            <form action="actions/update-imagem-usuario" method="POST" class="form" id="form" enctype="multipart/form-data">
+                <h3>Alterar Imagem do Post</h3>
+                <input type="hidden" name="idusuario" value="<?= $id ?>">
+
+                <div class="inputBox">
+                    <div>
+                        <input type="file" name="imagem"  class="box" accept=".jpeg, .jpg, .png, .gif, .webp">
+                        <?php if (!empty($usuario['imagem'])): ?>
+                        <p><img src="<?= BASE_SITE_URL . '/uploads/users/' . htmlspecialchars($usuario['imagem']) ?>" alt="<?= $usuario['nome'] ?>" height="250px" width="100%"></p>
+                         <?php endif; ?>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn">Alterar Imagem do Usuário</button>
             </form>
         </section>
     </main>
@@ -78,6 +155,5 @@
     </div>
 
     <script type="text/javascript" src="../assets/js/script.js"></script>
-    <script type="text/javascript" src="../assets/js/usuariosChart.js"></script>
 </body>
 </html>
